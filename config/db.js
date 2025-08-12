a3119fbe66ca71dfa3119a9fc1c5283e44c36974
Giancarlos <giancar85@hotmail.com>
2025-08-11 22:25:20 -0500

    Update conexion

--- config/db.js
+++ config/db.js
@@ -6,6 +6,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  connectTimeout: 10000 // 10s
 });
 
 module.exports = db;
